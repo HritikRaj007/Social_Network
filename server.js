7f35f7030754b50df4f8499a69f7fa61a83fa53c
Hritik Raj <rajhritik2000@gmail.com>
2021-07-02 15:53:57 +0530

    authentication added

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ dotenv.config()
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
