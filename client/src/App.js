f42994c245dfdda01f2603254c09c78e63ff6f73
Hritik Raj <rajhritik2000@gmail.com>
2021-07-06 11:47:11 +0530

    basic react setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+       <Navbar />
+       <Landing />
+    </div>
+  );
+}
+
+export default App;
