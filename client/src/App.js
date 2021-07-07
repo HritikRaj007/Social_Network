8409b50578de49a33558b49848624ac7a24ea5f0
Hritik Raj <rajhritik2000@gmail.com>
2021-07-07 14:45:45 +0530

    alert system completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import React from 'react';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+import Login from './components/auth/Login';
+import './App.css';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div>
-       <Navbar />
-       <Landing />
-    </div>
+    <Provider store={store}>
+    <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Alert/>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+  </Provider>
   );
 }
 
