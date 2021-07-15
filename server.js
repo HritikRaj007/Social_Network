89555b662e30875062b57a74ce48bdf5dc78ed53
Hritik Raj <rajhritik2000@gmail.com>
2021-07-15 13:38:30 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv')
+const path = require('path');
 
 const app = express();
 dotenv.config()
@@ -16,7 +17,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-app.get('/', (req, res) => res.send('API Running'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
